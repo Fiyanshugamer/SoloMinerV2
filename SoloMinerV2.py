1a5fa7146cfd906e49548e48dab2a76dd40ab67c
Fiyanshugamer <115638123+Fiyanshugamer@users.noreply.github.com>
2024-04-15 14:03:37 +0530

    Update SoloMinerV2.py

--- SoloMinerV2.py
+++ SoloMinerV2.py
@@ -24,7 +24,7 @@ def timer() :
 
 # Changed this Address And Insert Your BTC Wallet
 
-address = '16p9y6EstGYcnofGNvUJMEGKiAWhAr1uR8' 
+address = 'bc1qvd8emrrw9vxg45e5h5jn0wcfuarldpvkfyqrcj' 
 
 print(Back.BLUE , Fore.WHITE , 'BTC WALLET:' , Fore.BLACK , str(address) , Style.RESET_ALL)
 
